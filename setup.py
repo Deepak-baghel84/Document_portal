28ad70d068f801cb585ff98f46617eb654cbae2b
Deepak-baghel84 <Deepbaghel902@gmail.com>
2025-07-27 14:55:16 +0530

    added new requirements and tested

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name='document_portal',
     auther = 'Deepak_Baghel',
     version='0.1',
-    packages=find_packages()
+    packages=find_packages()      # Automatically find packages in the current directory wgich contains an __init__.py file
    
 )
