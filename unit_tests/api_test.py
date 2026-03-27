99d9d33f6acf6b1d9f7fcc6ee061ff42e05650b5
Deepak-baghel84 <Deepbaghel902@gmail.com>
2026-03-27 21:21:32 +0530

    testing is performed with testing files

---
+++ unit_tests/api_test.py
@@ -0,0 +1,20 @@
+import api.main as main
+from fastapi.testclient import TestClient
+import pytest
+
+
+client=TestClient(main.app)
+
+file_path="Data//Resume.pdf"
+def test_health_check():
+    response=client.get("/health")
+    assert response.status_code==200
+    assert response.json()=={"status":"ok","message":"Service is healthy","device":"document-portal"}
+
+def test_analyze_document():
+    with open(file_path, "rb") as f:
+        response=client.post("/analyze", files={"file": f})
+    assert response.status_code==200
+    assert isinstance(response.json(), dict)
+
+print("all okay")
