1b25dfc086089e81e5bf3d70a7ea319b2ca85e5b
Deepak-baghel84 <Deepbaghel902@gmail.com>
2026-04-19 23:38:10 +0530

    removed un necessary tests

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -10,10 +10,10 @@ def test_health_check():
     assert response.status_code==200
     assert response.json()=={"status":"ok","message":"Service is healthy","device":"document-portal"}
 
-def test_analyze_document():
-    with open(file_path, "rb") as f:
-        response=client.post("/analyze", files={"file": f})
-    assert response.status_code==200
-    assert isinstance(response.json(), dict)
+# def test_analyze_document():
+#     with open(file_path, "rb") as f:
+#         response=client.post("/analyze", files={"file": f})
+#     assert response.status_code==200
+#     assert isinstance(response.json(), dict)
 
 print("all okay")
